3cf7c6264fbda48d8b1041b8ced3e9e9ad871c57
Merziv <44921219+Merziv@users.noreply.github.com>
2018-11-28 20:38:59 +0100

    Uploading files added

--- src/main/java/com/wikitolatex/converter/Controller/MainController.java
+++ src/main/java/com/wikitolatex/converter/Controller/MainController.java
@@ -1,30 +1,89 @@
 package com.wikitolatex.converter.Controller;
 
-import javassist.NotFoundException;
+import com.wikitolatex.converter.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/converter")
 public class MainController {
 
-    private static final String EXTERNAL_FILE_PATH = ".\\ResourceFiles\\";
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    //private static final String EXTERNAL_FILE_PATH = ".\\ResourceFiles\\";
+
     @RequestMapping("/index")
     String index()
     {
         return "index page";
     }
+
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = fileStorageService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/converter/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        System.out.println("\nConversion successful!\nFile converted: " + fileName);
+
+        return new UploadFileResponse(fileName, fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.asList(files)
+                .stream()
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+    /*
+    Outdated code from last iteration
 
     //Displaying local pdf file in browser from "ResourceFiles" directory
     @RequestMapping("/file/{fileName:.+}")
@@ -62,6 +121,7 @@ public class MainController {
             }
         }
     }
+    */
 }
 
 /*
--- src/main/java/com/wikitolatex/converter/ConverterApplication.java
+++ src/main/java/com/wikitolatex/converter/ConverterApplication.java
@@ -1,8 +1,14 @@
 package com.wikitolatex.converter;
 
+import com.wikitolatex.converter.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class ConverterApplication {
 
     public static void main(String[] args) {
